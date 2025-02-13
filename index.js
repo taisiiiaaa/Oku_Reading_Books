de835510acad7956b799630d3dec148255bd4d4c
Taisiia <shloma20082004@icloud.com>
2025-02-13 17:05:33 +0100

    fixed minoe flaws

--- index.js
+++ index.js
@@ -6,19 +6,16 @@ document.querySelector("#cross_icon").addEventListener("click", function(event) 
     }
 });
 
-
 document.addEventListener("DOMContentLoaded", function () {
     const elements = document.querySelectorAll(".library, .reading_goal, .favourite_book, .statistics, .collections, .community_reviews");
 
     if (elements.length > 0) {
         elements.forEach(element => {
-            // Add active class when touched
             element.addEventListener("touchstart", function () {
                 element.classList.add("active");
             });
         });
 
-        // Remove active class when scrolling (for all elements at once)
         const removeActiveClasses = () => {
             elements.forEach(element => element.classList.remove("active"));
         };
